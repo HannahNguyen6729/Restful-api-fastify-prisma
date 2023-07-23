e23faa0147b0c73ae23e5b22bc4d3af5dfa14aaa
Hanh Nguyen <myhanh.nguyen.969317598@gmail.com>
2023-07-23 10:26:08 +0300

    build json schema for product

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import fastify, { FastifyReply, FastifyRequest } from "fastify";
 import fastifyHealthcheck from "fastify-healthcheck";
 import autoLoad from "@fastify/autoload";
 import { userSchemas } from "./modules/user/user.schema";
+import { productSchemas } from "./modules/product/product.schema";
 import fjwt from "@fastify/jwt";
 
 const server = fastify({
@@ -32,13 +33,18 @@ const main = async () => {
   );
 
   await server.register(fastifyHealthcheck);
-
-  for (const schema of userSchemas) {
+  const x = [...userSchemas, ...productSchemas];
+  for (const schema of x) {
     await server.addSchema(schema);
   }
 
   await server.register(autoLoad, {
     dir: join(__dirname, "modules/user"),
+    ignorePattern: /.*(test|spec|.controller|.service).(ts|js)/,
+  });
+
+  await server.register(autoLoad, {
+    dir: join(__dirname, "modules/product"),
     ignorePattern: /.*(test|spec|.controller|.service).(ts|js)/,
   });
 
---
+++ src/modules/product/product.schema.ts
@@ -0,0 +1,30 @@
+import { z } from "zod";
+import { buildJsonSchemas } from "fastify-zod";
+
+const productSchema = z.object({
+  title: z.string(),
+  price: z.string(),
+  content: z.string().optional(),
+});
+
+const productResponseSchema = z.object({
+  title: z.string(),
+  price: z.string(),
+  content: z.string().optional(),
+  id: z.number(),
+  createAt: z.string(),
+  updateAt: z.string(),
+});
+
+const productsResponseSchema = z.array(productResponseSchema);
+
+export type CreateProductInput = z.infer<typeof productSchema>;
+
+export const { schemas: productSchemas, $ref } = buildJsonSchemas(
+  {
+    productSchema,
+    productResponseSchema,
+    productsResponseSchema,
+  },
+  { $id: "MyProductSchema" }
+);
