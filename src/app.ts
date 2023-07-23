e2daf255de93f6cb6ff3e335c409a21b7398ecfb
Hanh Nguyen <myhanh.nguyen.969317598@gmail.com>
2023-07-23 16:28:16 +0300

    add swagger

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,9 @@ import autoLoad from "@fastify/autoload";
 import { userSchemas } from "./modules/user/user.schema";
 import { productSchemas } from "./modules/product/product.schema";
 import fjwt from "@fastify/jwt";
+import fswagger from "@fastify/swagger";
+import swaggerUI from "@fastify/swagger-ui";
+import { version } from "../package.json";
 
 const server = fastify({
   logger: true,
@@ -48,6 +51,46 @@ const main = async () => {
     await server.addSchema(schema);
   }
 
+  await server.register(fswagger, {
+    swagger: {
+      info: {
+        title: "Fastify-prisma",
+        description: "Fastify API",
+        version,
+      },
+      externalDocs: {
+        url: "https://swagger.io",
+        description: "Find more info here",
+      },
+      host: `http://localhost:3000/documentation/static/index.html`,
+      schemes: ["http"],
+      consumes: ["application/json"],
+      produces: ["application/json"],
+    },
+  });
+
+  await server.register(swaggerUI, {
+    routePrefix: "/documentation",
+    uiConfig: {
+      docExpansion: "full",
+      deepLinking: false,
+    },
+    uiHooks: {
+      onRequest: function (_request, _reply, next) {
+        next();
+      },
+      preHandler: function (_request, _reply, next) {
+        next();
+      },
+    },
+    staticCSP: true,
+    transformStaticCSP: (header) => header,
+    transformSpecification: (swaggerObject) => {
+      return swaggerObject;
+    },
+    transformSpecificationClone: true,
+  });
+
   await server.register(autoLoad, {
     dir: join(__dirname, "modules/user"),
     ignorePattern: /.*(test|spec|.controller|.service).(ts|js)/,
@@ -59,6 +102,7 @@ const main = async () => {
   });
 
   await server.listen({ port: 3000, host: "0.0.0.0" }, (err, address) => {
+    server.swagger();
     console.log(`server listening on port ${address}`);
     if (err) {
       console.error(err);
